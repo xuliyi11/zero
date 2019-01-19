437691ef551003743cae5318de56ae39df932a8a
xuliyi <395355324@qq.com>
2019-01-19 10:36:54 +0800

    添加线程池配置

---
+++ zero-master/src/main/java/com/controller/OrderController.java
@@ -0,0 +1,13 @@
+package com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class OrderController {
+
+    public void getOrderInfo(@RequestParam String orderId) {
+
+    }
+
+}
